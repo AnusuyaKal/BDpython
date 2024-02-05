7118267449cb4f684a8cf85ea561d9dd6dd0cc86
AnusuyaKal <k_anusuya123@yahoo.com>
2024-02-05 20:02:02 +0000

    commit

--- argu.py
+++ argu.py
@@ -1,7 +1,8 @@
-def argu(height,*arg,**kwarg ):
-    print(''Height of the triangle is '' height)
-    for x in args:
+def argu(height,*arg,**kwarg):
+    print("Height of the triangle is")
+    for x in arg:
         print(x)
     for kw in kwarg:
         print(kw, kwarg[kw])
             
+argu(100,"python is good program")
