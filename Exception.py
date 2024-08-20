13d36c265b73f63926914e4e40977d2fb1b03500
AnusuyaKal <159040029+AnusuyaKal@users.noreply.github.com>
2024-08-20 19:35:15 +0100

    Delete Exception.py

--- Exception.py
+++
@@ -1,8 +0,0 @@
-def divide(a, b):
-    try:
-        result = a / b
-    except ZeroDivisionError:
-        print("Error: Cannot divide by zero")
-        return None
-    return result
-divide(8,0)
