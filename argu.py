b2bd63e367fa6690e771e4e01494081f737ea6c2
AnusuyaKal <159040029+AnusuyaKal@users.noreply.github.com>
2024-08-20 19:42:44 +0100

    Delete argu.py

--- argu.py
+++
@@ -1,8 +0,0 @@
-def argu(i,*arg,**kwarg):
-    print(i, end='')
-    for x in arg:
-        print(x)
-    for kw in kwarg:
-        print(kw, ':', kwarg[kw])
-            
-argu("python"," is a simple program", use=" to use in Big Data", oops="also it is a object oriented program")
